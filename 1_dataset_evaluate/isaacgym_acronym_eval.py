1e05811c895520a00dcf35dd84da5fec69f6a7b5
red0orange <huangdehao919@gmail.com>
2024-09-22 13:08:26 +0800

    1. isaac gym 大概率能够有效评估 Acronym 的 success 抓取。能够对 data_dict 进行批量评估了

--- 1_dataset_evaluate/isaacgym_acronym_eval.py
+++ 1_dataset_evaluate/isaacgym_acronym_eval.py
@@ -18,7 +18,6 @@ if __name__ == '__main__':
     test_data_path = "/home/red0orange/Projects/handgrasp_ws/tmp.npy"
     data_dict = np.load(test_data_path, allow_pickle=True).item()
 
-
     viser_grasp = ViserForGrasp()
 
     device = "cuda:0"
@@ -32,28 +31,6 @@ if __name__ == '__main__':
 
         grasp_Ts = [grasp_Ts[i] for i in range(len(grasp_Ts)) if grasp_success[i]]
 
-        # if True:
-        if False:
-            mesh = o3d.io.read_triangle_mesh(obj_mesh_path)
-            mesh = mesh.scale(mesh_scale, center=[0, 0, 0])
-            mesh = mesh.transform(mesh_T)
-            pc = np.array(mesh.vertices)
-
-            # grasp_success = data['grasp_success']
-            # success_grasp_Ts = np.where(grasp_success)[0]
-            # success_grasp_Ts = [grasp_Ts[i] for i in success_grasp_Ts]
-            success_grasp_Ts = grasp_Ts
-            print(f"Success grasp num: {len(success_grasp_Ts)}")
-            for i in range(len(success_grasp_Ts)):
-                print("current grasp id: ", i)
-                grasp_T = success_grasp_Ts[i]
-                viser_grasp.vis_grasp_scene(max_grasp_num=1, pc=pc, grasp_Ts=[grasp_T], mesh=mesh)
-                viser_grasp.wait_for_reset()
-
-            viser_grasp.vis_grasp_scene(max_grasp_num=40, pc=pc, grasp_Ts=grasp_Ts, mesh=mesh)
-            viser_grasp.wait_for_reset()
-
-
         tmp_obj_mesh_path = os.path.join(cache_dir, "tmp_{}.obj".format(data_id))
         mesh = o3d.io.read_triangle_mesh(obj_mesh_path)
         mesh = mesh.scale(mesh_scale, center=[0, 0, 0])
@@ -61,11 +38,29 @@ if __name__ == '__main__':
 
         mesh_center = mesh.get_center()
         mesh.translate(-mesh_center)
+        o3d.io.write_triangle_mesh(tmp_obj_mesh_path, mesh)
+
         for i, grasp_T in enumerate(grasp_Ts):
             grasp_Ts[i][:3, 3] -= mesh_center
 
-        o3d.io.write_triangle_mesh(tmp_obj_mesh_path, mesh)
+        if True:
+        # if False:
+            pc = np.array(mesh.vertices)
+            print(f"Grasp num: {len(grasp_Ts)}")
+            for i in range(len(grasp_Ts)):
+                print("current grasp id: ", i)
+                grasp_T = grasp_Ts[i]
 
+                # @note
+                grasp_T = update_pose(grasp_T, translate=[0, 0, 0.08])
+
+                viser_grasp.vis_grasp_scene(max_grasp_num=1, pc=pc, grasp_Ts=[grasp_T], mesh=mesh)
+                viser_grasp.wait_for_reset()
+
+            viser_grasp.vis_grasp_scene(max_grasp_num=40, pc=pc, grasp_Ts=grasp_Ts, mesh=mesh)
+            viser_grasp.wait_for_reset()
+
+        # @note
         grasp_Ts = grasp_Ts[:20]
 
         scales = [1.0] * len(grasp_Ts)
