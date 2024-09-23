c698816484bb85b2282845bf0afe8d066a02c4bc
red0orange <huangdehao919@gmail.com>
2024-09-23 16:54:39 +0800

    1. 将放弃的方案相关的代码全部放到 handgrasp_ws_bak 里 2. 添加 graspnet-baseline 核心代码，还没跑通

--- 3rd_multigrippergrasp_examples/convert_obj_to_usd.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-/home/red0orange/.local/share/ov/pkg/isaac-sim-4.0.0/isaac-sim.sh /home/red0orange/Projects/handgrasp_ws/multigrippergrasp_examples/my_eva_convert_obj_to_usd.py
--- 3rd_position-contrained-6dof-graspnet/dataset/download_dataset.sh
+++
@@ -1,3 +0,0 @@
-wget https://huggingface.co/datasets/jens-lundell/cong/resolve/main/full_dataset.zip -O $1"full_dataset.zip"
-unzip -n -q $1"full_dataset.zip" -d $1"/dataset/"
-rm $1"full_dataset.zip"
